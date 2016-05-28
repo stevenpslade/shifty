41a423db62e85cadccbdc30db584abb576fba857
stevenpslade <stevenpslade@gmail.com>
2016-05-28 02:07:49 -0700

    adding validations and showing errors on form when errors occur

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -1,2 +1,13 @@
 class Shift < ActiveRecord::Base
+  validates :start_date, presence: true
+  validates :day_shift, presence: true
+  validate :start_date_is_future
+
+  protected
+
+  def start_date_is_future
+    if start_date.present?
+      errors.add(:start_date, "should be in the future") if start_date < Date.today
+    end
+  end
 end
