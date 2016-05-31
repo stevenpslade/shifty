56013e35bb8dd8e80e7badc2c62ac65df4dab0ce
stevenpslade <stevenpslade@gmail.com>
2016-05-31 11:40:06 -0700

    removing valiation for precense true for user as user_id is given to shift in create method

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -1,7 +1,6 @@
 class Shift < ActiveRecord::Base
   belongs_to :user
 
-  validates :user, presence: true
   validates :start_date, presence: true
   validates :day_shift, :inclusion => {:in => [true, false]}
   validate :start_date_is_future
