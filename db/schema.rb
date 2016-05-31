04a39b1a8f41bf7a3cbe8900827d7123ae96c234
stevenpslade <stevenpslade@gmail.com>
2016-05-30 22:07:38 -0700

    updating schema for shifts to have user reference

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160529063759) do
+ActiveRecord::Schema.define(version: 20160531033114) do
 
   create_table "shifts", force: :cascade do |t|
     t.datetime "start_date"
@@ -19,7 +19,10 @@ ActiveRecord::Schema.define(version: 20160529063759) do
     t.string   "notes"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "shifts", ["user_id"], name: "index_shifts_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "provider",   null: false
